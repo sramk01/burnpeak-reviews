9196514c509bf613da1aa3f91c10d429b9e8aa95
sramk01 <ramkriti.twds@gmail.com>
2025-12-19 17:20:48 +0530

    Delete burn-peak-supplement.com directory

--- burn-peak-supplement.com/assets/js/myscript.js
+++
@@ -1,7 +0,0 @@
-const faqs = document.querySelectorAll('.faq');
-
-faqs.forEach(faq => {
-  faq.querySelector('.faq-question').addEventListener('click', () => {
-    faq.classList.toggle('open');
-  });
-});
